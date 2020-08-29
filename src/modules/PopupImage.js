29c1ccc08103463bb28653e8ed17cba99fb5029a
TolkachevPeter <peter.tolkachev@gmail.com>
2020-08-29 20:37:54 +0300

    Updates

--- src/modules/PopupImage.js
+++
@@ -1,8 +0,0 @@
-import Popup from './Popup';
-
-export default class PopupImage extends Popup {
-    open(url) {
-      this.popup.querySelector(".popup__image").setAttribute(`src`, url);
-      super.open();
-      }
-}
